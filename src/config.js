7b0df1df6c17fbddcb4306130c5f495c8355d76a
Samir Chowdhury <87253770+samirhcy@users.noreply.github.com>
2026-02-19 13:22:29 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -27,21 +27,21 @@ export const Config = {
   audio: {
     // These paths match your original game.html. 
     // Ensure the files are in the corresponding locations in your folder.
-    bgm: ["/assets/bgm1.mp3", "/assets/bgm4.mp3", "/assets/bgm3.mp3"],
+    bgm: ["assets/bgm1.mp3", "assets/bgm4.mp3", "assets/bgm3.mp3"],
     sfx: {
-      term_open: "/assets/term1.mp3",
-      term_close: "/assets/term2.mp3",
-      term_success: "/assets/term_success.mp3",
-      obj_toggle: "/assets/obj.mp3",
-      obj_update: "/assets/obj2.mp3",
+      term_open: "assets/term1.mp3",
+      term_close: "assets/term2.mp3",
+      term_success: "assets/term_success.mp3",
+      obj_toggle: "assets/obj.mp3",
+      obj_update: "assets/obj2.mp3",
       log: "",
-      intro_hit: "/assets/on2.wav",
+      intro_hit: "assets/on2.wav",
       story_ambience: "",
-      profile_switch: "/assets/profile.mp3",
-      player_hit: "/assets/term.mp3",
-      player_shoot: "/assets/attack.mp3",
-      shield_up: "/assets/sh1.mp3",
-      shield_down: "/assets/sh2.mp3",
+      profile_switch: "assets/profile.mp3",
+      player_hit: "assets/term.mp3",
+      player_shoot: "assets/attack.mp3",
+      shield_up: "assets/sh1.mp3",
+      shield_down: "assets/sh2.mp3",
     },
   },
 };
